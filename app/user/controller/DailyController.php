11e9e20ab78df75a583746ac6ae434ada9edd16c
LovelyWei <1027514847@qq.com>
2017-10-31 23:25:10 +0800

    Add new features
    睡觉前敬业

---
+++ app/user/controller/DailyController.php
@@ -0,0 +1,21 @@
+<?php
+namespace app\user\controller;
+
+use cmf\controller\UserBaseController;
+
+class DailyController extends UserBaseController
+{
+	public function checkin()
+	{
+		if (''==input('id'))
+		{
+			$this->error('id不能为空!');
+		}
+		else
+		{
+			$info = db('user')->where('id',input('id'))->find();
+			db('daily')->insert(['userid'=>input('id'),'username'=>$info['user_login'],'date'=>'NOW()']);
+			$this->success('签到成功!');
+		}
+	}
+}
